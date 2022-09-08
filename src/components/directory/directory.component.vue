d4f4bf30aedeb4aac9dd300fdcab523b740bacb0
Max Miliano <milianordelcanto@gmail.com>
2022-09-08 18:28:41 -0300

    first commit

---
+++ src/components/directory/directory.component.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import CategoryItemComponent from '../category-item/category-item.component.vue'
+</script>
+
+<script lang="ts">
+interface Category {
+  id: number
+  title: string
+  imageUrl: string
+}
+
+export default {
+  props: {
+    categories: Array<Category>,
+  },
+}
+</script>
+
+<template>
+  <div class="categories-container">
+    <CategoryItemComponent
+      v-for="category in categories"
+      :key="category.id"
+      :title="category.title"
+      :imageUrl="category.imageUrl"
+    />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.categories-container {
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+</style>
---
+++ src/views/home.view.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import DirectoryComponent from '../components/directory/directory.component.vue'
+import { categories } from '../utils'
+</script>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      categories,
+    }
+  },
+}
+</script>
+
+<template>
+  <DirectoryComponent :categories="categories" />
+</template>
